fa16db2ee427234238120f6779871726f9737282
Oukomondi <oukomondi@gmail.com>
2022-09-12 16:12:56 -0700

    Print all numbers between 0 and 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - main function
+ * 
+ * Return: 0 when successful
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%i", i);
+	}
+	putchar('\n');
+
+	return (0);
+}
+
