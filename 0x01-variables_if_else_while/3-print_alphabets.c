8eb8da4461837c61d9d2ee87d63223fd8cfe5d81
Oukomondi <oukomondi@gmail.com>
2022-09-12 15:41:26 -0700

    Code prints alphabet in small and capital letters

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdio.h>
+#include <stdio.h>
+/**
+ *  * main - Entry point
+ *
+ *  *
+ *
+ *  * Return: Always 0 (Success)
+ *
+**/
+int main(void)
+{
+char low;
+for (low = 'a'; low <= 'z'; low++)
+putchar(low);
+for (low = 'A'; low <= 'Z'; low++)
+putchar(low);
+putchar('\n');
+return (0);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
