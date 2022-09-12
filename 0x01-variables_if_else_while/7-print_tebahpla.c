3c3dc1e78950d98b1d87851d48f1f9334b070fbf
Oukomondi <oukomondi@gmail.com>
2022-09-12 16:48:15 -0700

    Print alphabet in lower case, backwards

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - main entry point
+ * 
+ * Return: success returns 0
+ */
+int main(void)
+{
+	char x;
+
+	for (x = 'z'; x >= 'a'; x--)
+	{
+		putchar(x);
+	}
+	putchar('\n');
+	return (0);
+}
