709715171d51491856646f1eb9d807e304bff75d
Oukomondi <oukomondi@gmail.com>
2022-09-14 02:20:55 -0700

    Prints _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - this is  the main entry to the code
+ *
+ * Return: returns the value zero in success
+ */
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+        putchar('a');
+	putchar('r');
+	putchar('\n');
+
+	return 0;
+}
