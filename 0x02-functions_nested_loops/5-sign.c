9d927bb156c098d822d95a3400b20218f0f9a3a5
Oukomondi <oukomondi@gmail.com>
2022-09-14 23:34:15 -0700

    Function checks for signs to characters

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_sign - function assigns signs
+ *
+ *@s: character to manipulate
+ *
+ * Return: 1 is n is +ve, 0 if 0 and -1 if -ve
+ */
+
+int print_sign (int s)
+{
+	if (s>0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (s==0)
+	{
+		_putchar ('0');
+		return (0);
+	}
+	else 
+	{
+		_putchar ('-');
+		return (-1);
+	}
+}
