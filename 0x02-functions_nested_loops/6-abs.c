bd38cc7020aa9d0c9dd8effc51320f8aab89c9da
Oukomondi <oukomondi@gmail.com>
2022-09-15 08:13:46 -0700

    Function checks for absolute value of an integer

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _abs -checks for absolute value of int
+ *
+ * @n -the executible character
+ *
+ * Return: 0 to return the absolute value
+ * of an inter
+ */
+
+int _abs(int n)
+{
+	if (n > 0)
+	{
+		return (n);
+	}
+	else if (n == 0)
+	{
+		return (0);
+	}
+	else
+	{
+		return (-n);
+	}
+}
