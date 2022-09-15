1bbdeadfc6b48a8c09c89da3be3ab14e946d833b
Oukomondi <oukomondi@gmail.com>
2022-09-14 22:36:22 -0700

    Hot fixes

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,7 +2,7 @@
 
 /**
  * main - entry into the code
- * Description: print_alphabet_x10 prints the alphabet 10 times
+ * print_alphabet_x10 -prints the alphabet 10 times
  * Return: always zero
  */
 
@@ -10,15 +10,14 @@ void print_alphabet_x10(void)
 {
 	char a = 'a';
 	int n = 10;
-
-	while (n <= 0)
+	while (n >= 1)
 	{
 		while (a <= 'z')
 		{
 			_putchar(a);
 			++a;
 		}
-		++n;
+		--n;
 		_putchar(10);
 		a = 'a';
 	}
