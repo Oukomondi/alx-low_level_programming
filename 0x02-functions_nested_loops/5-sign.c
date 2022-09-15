a9dcb4788f4114a93e9df61948d63cc5d72ae64d
Oukomondi <oukomondi@gmail.com>
2022-09-14 23:37:37 -0700

    Fixed a bug

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,17 +9,17 @@
 
 int print_sign (int s)
 {
-	if (s>0)
+	if (s > 0)
 	{
 		_putchar('+');
 		return (1);
 	}
-	else if (s==0)
+	else if (s == 0)
 	{
 		_putchar ('0');
 		return (0);
 	}
-	else 
+	else
 	{
 		_putchar ('-');
 		return (-1);
