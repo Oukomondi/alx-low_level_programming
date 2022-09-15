2e5798f2027eaa27eb5de3751bd9fa36eed5ba3e
Oukomondi <oukomondi@gmail.com>
2022-09-14 23:40:15 -0700

    Fixed a bug

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -7,21 +7,21 @@
  * Return: 1 is n is +ve, 0 if 0 and -1 if -ve
  */
 
-int print_sign (int s)
+int print_sign(int s)
 {
-	if (s > 0)
+	if(s > 0)
 	{
 		_putchar('+');
 		return (1);
 	}
-	else if (s == 0)
+	else if(s == 0)
 	{
-		_putchar ('0');
-		return (0);
+		_putchar('0');
+		return(0);
 	}
 	else
 	{
-		_putchar ('-');
-		return (-1);
+		_putchar('-');
+		return(-1);
 	}
 }
