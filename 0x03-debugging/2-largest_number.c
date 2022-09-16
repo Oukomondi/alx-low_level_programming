b89fa5b900d164700fbb351de0e1a6774b0fc01f
Oukomondi <oukomondi@gmail.com>
2022-09-16 10:45:24 -0700

    Bug fixes

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,20 +10,21 @@
 
 int largest_number(int a, int b, int c)
 {
-    int largest;
+	int largest;
 
-    if (a > b && b > c)
-    {
-        largest = a;
-    }
-    else if (b > a && a > c)
-    {
-        largest = b;
-    }
-    else if (c > a && a > b)
-    {
-        largest = c;
-    }
+	if ((a > b) && (b > c))
+	{
+		largest = a;
+	}
 
-    return (largest);
+	else if ((b > a) && (a > c))
+	{
+		largest = b;
+	}
+	else if ((c > a) && (a > b))
+	{
+		largest = c;
+	}
+
+	return (largest);
 }
