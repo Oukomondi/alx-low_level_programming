8ae4956e388c3299058c91af5e4cda9cb2b12f13
Oukomondi <oukomondi@gmail.com>
2022-09-20 13:04:28 -0700

    Copy string into another

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+  * _strcpy - Function copies a string
+  * @dest: value of destination variable
+  * @src: Source value
+  *
+  * Return: the pointer to dest
+  */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	dest[i++] = '\0';
+
+	return (dest);
+}
