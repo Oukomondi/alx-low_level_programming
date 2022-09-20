b203fedd9e9d6ccae94326f9382e1bf3177b7477
Oukomondi <oukomondi@gmail.com>
2022-09-20 07:44:40 -0700

    Function that assigns function to pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * reset_to_98 - takes a pointer to an int
+ *               paratemeter and updates 
+ *               the value it points to 98
+ * @n: Pointer arg passed to function
+ *
+ * Return: Always 0
+ */
+
+void reset_to_98(int *n)
+
+{
+ *n =98;
+}
+
+
+
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+Writing Pointers and Arrays
