5c5e96ca2e211078a300216e8999d121597c1fce
Oukomondi <oukomondi@gmail.com>
2022-09-20 07:54:37 -0700

    Bug fix

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * reset_to_98 -takes a pointer to an int
- *		-patemeter and updates 
+ *		-patemeter and updates
  *		-the value it points to 98
  * @n: Pointer arg passed to function
  *
