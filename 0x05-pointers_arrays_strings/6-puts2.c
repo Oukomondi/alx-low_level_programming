17ea93a8d3e7152ac6e8d6a146cec7cfc58eeb93
Oukomondi <oukomondi@gmail.com>
2022-09-20 12:12:46 -0700

    Print every character in a string followed by new line

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <string.h>
+
+/**
+* puts2 - Function prints every character in a string
+*	-starting with first character followed by 
+*	-new line
+*
+* @str: string parameter to be passed nto arg
+*/
+void puts2(char *str)
+{
+	int len, i;
+
+	len = strlen(str);
+	for (i = 0; i < len; i += 2)
+		_putchar(str[i]);
+	_putchar('\n');
+}
