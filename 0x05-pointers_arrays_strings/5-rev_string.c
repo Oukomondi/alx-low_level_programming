c984798a6a4a98af8d51ce1f5e0e5120d04ec110
Oukomondi <oukomondi@gmail.com>
2022-09-20 12:06:28 -0700

    Function reverses a string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+* rev_string -> function reverses a string
+* @s: parameter s
+*/
+void rev_string(char *s)
+{
+	char tmp;
+	int i, length1, length2;
+
+	length1 = 0;
+	length2 = 0;
+
+	while (s[length1] != '\0')
+		length1++;
+
+	length2 = length1 - 1;
+
+	for (i = 0; i < length1 / 2; i++)
+	{
+		tmp = s[i];
+		s[i] = s[length2];
+		s[length2] = tmp;
+		length2 -= 1;
+	}
+}
